ef193812b8337a810d6c5b22838fe0cf11c9940f
Josh Spangler <jspangl3@vols.utk.edu>
2019-09-24 15:54:19 -0400

    Added commented code for more ingredients/directions, not working yet #16

--- new_recipe.js
+++ new_recipe.js
@@ -15,6 +15,25 @@ ipcRenderer.on('response', (event, arg) => {
 //gets values out of text fields and sends them to main
 document.getElementById('add_recipe').addEventListener('click', function(){
     let recipe_name = document.getElementById('recipe_name').value;
+    
+    /*let ingredients = [];
+    let directions = [];
+    let i = 1;
+    let ing = "ingredients";
+    let dir = "directions";
+	 
+    while(document.getElementById(str.concat(ing, i.toString())).value != null){
+	ingredients[i-1] = document.getElementById(str.concat(ing, i.toString())).value;
+	i++;
+    }
+    
+    i = 1;
+
+    while(document.getElementById(str.concat(dir, i.toString())).value != null){
+    	directions[i-1] = document.getElementById(str.concat(dir, i.toString())).value;
+	i++;
+    }	    
+	*/
     let ingredients = document.getElementById('ingredients').value;
     let directions = document.getElementById('directions').value;
     ipcRenderer.send('recipe', recipe_name, ingredients, directions);
