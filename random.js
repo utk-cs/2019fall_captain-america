e028189e44185a2eb732899007edd484a2532891
Mac <ddevrie3@vols.utk.edu>
2019-10-01 12:51:07 -0400

    Making random recipe button open up a new html file, and adding next recipe and previous recipe buttons closes #24

---
+++ random.js
@@ -0,0 +1,7 @@
+//file that renderers events and sends them to main
+const { ipcRenderer } = require('electron')
+
+//sends message to main for debugging
+function log(message){
+    ipcRenderer.send('log', message)
+}
