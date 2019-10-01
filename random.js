5a6ee3d3b2bffdf29497ff3b18a398f2833ee3da
Mac <ddevrie3@vols.utk.edu>
2019-10-01 13:48:36 -0400

    Adding random recipe button functionality. Now it will display a random recipe from the map. fixes #25

--- random.js
+++ random.js
@@ -5,3 +5,17 @@ const { ipcRenderer } = require('electron')
 function log(message){
     ipcRenderer.send('log', message)
 }
+
+//listens for button to be clicked and requests data form main
+document.getElementById('display_random_recipe').addEventListener('click', function(){
+    ipcRenderer.send('random_recipe', "");
+})
+//displays random recipe into html file
+ipcRenderer.on('random_recipe_return', (event, arg ) => {
+    let recipename = document.getElementById('recipename');
+    let ingredients = document.getElementById('ingredients');
+    let directions = document.getElementById('directions');
+    recipename.textContent = arg.recipename;
+    ingredients.textContent = "Ingredients: " + arg.ingredients;
+    directions.textContent = "Directions: " + arg.directions; 
+})
