16db7ebc3118ac9e3226c24dfd646f0591b3403a
Catherine Fei <cfei1@vols.utk.edu>
2019-10-30 19:06:58 -0400

    app now automatically launches maximized #51

--- main.js
+++ main.js
@@ -99,13 +99,12 @@ app.on('ready', () => {
 
     // This creates a new BrowserWindow and sets win to be a reference to that new window.
     win = new BrowserWindow({
-        width: 600, // Sets the width by pixel count
-        height: 400, // Sets the height by pixel count
         webPreferences: {
             nodeIntegration: true, // This ie extremely important later, but is not used in this example.
             devTools: true,
         }
     });
+    win.maximize();
     // This is a method of a BrowserWindow that lets you load an html file to view.
     win.loadFile('basic.html');
 
