e1e40cc4e148946c98d9a5544bb67af83eb4f9b7
Dylan DeVries <ddevrie3@vols.utk.edu>
2019-11-20 08:35:14 -0500

    making ingredients and directions fields dynamicallly generate on page closes #71

--- new_recipe.js
+++ new_recipe.js
@@ -1,5 +1,6 @@
 //file that renderers events and sends them to main
-const { ipcRenderer } = require('electron')
+const { ipcRenderer } = require('electron');
+window.$ = window.jQuery = require('jquery');
 
 //sends message to main for debugging
 function log(message){
@@ -28,44 +29,68 @@ ipcRenderer.on('recipe_exists', (event, arg) => {
 //listens for add recipe button to be clicked
 //gets values out of text fields and sends them to main
 document.getElementById('add_recipe').addEventListener('click', function(){
-    let recipe_name = document.getElementById('recipe_name').value;
-    
-    let ingredients = [];
-    let directions = [];
-    let ing = 'ingredients'; 
-    let dir = 'directions';
-    let i = 1;
-    let ingString = ing.concat(i.toString());
-    let dirString = dir.concat(i.toString());
+    var directions = [];
+    var ingredients = [];
 
+    var j = 0;
+    $.each($('.dir_text'), function(i, item) {
+        if($(item).val() !== null){
+             directions[j] = $(item).val(); 
+            j = j + 1;
+        }
+    });
+
+    j = 0;
+    $.each($('.ing_text'), function(i, item) {
+        if($(item).val() !== null){
+             ingredients[j] = $(item).val(); 
+            j = j + 1;
+        }
+    });
+
+    let recipe_name = document.getElementById('recipe_name').value;
     let origin = document.getElementById('origin').value;
     let prep = document.getElementById('prep').value;
     let course = document.getElementById('course').value;
     let img = document.getElementById('img').value.replace("C:\\fakepath\\", "");
     //let img = document.getElementById('img').value
-    
-    while(document.getElementById(ingString).value !== ''){
-	ingredients[i-1] = document.getElementById(ingString).value;
-	i++;
-
-	if(i > 10){
-		break;
-	}
-
-	ingString = ing.concat(i.toString());
-    }
-
-    i = 1;
-    
-    while(document.getElementById(dirString).value !== ''){    
-	directions[i-1] = document.getElementById(dirString).value;
-        i++;
-
-	if(i > 10){
-		break;
-	}
-
-	dirString = dir.concat(i.toString());
-    }
     ipcRenderer.send('recipe', recipe_name, ingredients, directions, origin, prep, course, img);
 })
+
+$(document).ready(function() {
+	var max_fields      = 10; //maximum input boxes allowed
+	var wrapper   		= $(".input_fields_wrap"); //Fields wrapper
+	var add_button      = $(".add_field_button"); //Add button ID
+	
+	var x = 1; //initlal text box count
+	$(add_button).click(function(e){ //on add input button click
+		e.preventDefault();
+		if(x < max_fields){ //max input box allowed
+			x++; //text box increment
+			$(wrapper).append('<div><input type="text" class = "dir_text"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+		}
+	});
+	
+	$(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+		e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
+
+$(document).ready(function() {
+	var max_fields      = 10; //maximum input boxes allowed
+	var wrapper   		= $(".input_fields_wrap1"); //Fields wrapper
+	var add_button      = $(".add_field_button1"); //Add button ID
+	
+	var x = 1; //initlal text box count
+	$(add_button).click(function(e){ //on add input button click
+		e.preventDefault();
+		if(x < max_fields){ //max input box allowed
+			x++; //text box increment
+			$(wrapper).append('<div><input type="text" class = "ing_text"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+		}
+	});
+	
+	$(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+		e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
